b73bd0fd0058b9bd09f8142d3395d9b1ac28c9ac
Vincent Palomo <vincentpalomo@yahoo.com>
2024-12-22 18:16:55 -0600

    fix: Update API URL in environment configuration
    - Changed the API URL in the environment file to remove the port number, ensuring the application points to the correct backend server without specifying the port during deployment.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://localhost:5000'  // Assuming Flask runs on port 5000
-  apiUrl: 'http://backend.vincentpalomo.dev:5000'
+  apiUrl: 'http://backend.vincentpalomo.dev'
 }; 
